5f69fbad2706887d84abf96285282999303eeb20
Tanvir Rahman <tanvir.rahman.ewu@gmail.com>
2020-06-07 11:52:26 +0600

    node example

---
+++ node-examples/index.js
@@ -0,0 +1,22 @@
+var rect = {
+    perimeter : (x, y) => (2*(x+y)),
+    area: (x, y) => x*y
+
+}
+
+function solveRect(l, b)
+{
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+
+    if( l <= 0 || b <= 0)
+    {
+        console.log("Invalid input")
+    }
+    else{
+        console.log("Area : " + rect.area(l, b))
+
+        console.log("Perimeter : " + rect.perimeter(l, b));
+    }
+}
+
+solveRect(10, 10)
