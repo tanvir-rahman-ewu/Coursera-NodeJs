156b9c613d8f954edd32cac33fe4f58e48b7bb4d
Tanvir Rahman <tanvir.rahman.ewu@gmail.com>
2020-06-07 12:52:03 +0600

    node callback and error handaling

--- node-examples/index.js
+++ node-examples/index.js
@@ -4,15 +4,16 @@ function solveRect(l, b)
 {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if( l <= 0 || b <= 0)
-    {
-        console.log("Invalid input")
-    }
-    else{
-        console.log("Area : " + rect.area(l, b))
-
-        console.log("Perimeter : " + rect.perimeter(l, b));
-    }
+    rect(l, b, (err, rectangle) => {
+        if(err)
+        {
+            console.log("Error: ", err.message)
+        }
+        else{
+            console.log("Area: " + rectangle.area())
+        }
+    })
+    console.log("this statement is after call to rect()")
 }
 
 solveRect(10, 10)
